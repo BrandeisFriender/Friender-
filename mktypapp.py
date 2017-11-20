bcd5c9a8a5032f0c28391968a4ab9d6e559fed62
DarioAlves <33233766+DarioAlves@users.noreply.github.com>
2017-11-20 14:40:08 -0500

    adding chat

--- mktypapp.py
+++ mktypapp.py
@@ -36,6 +36,10 @@ def survey():
 def formdemo():
 	return render_template('formdemo.html')
 
+@app.route('/chat')
+def formdemo():
+	return render_template('chat.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=3000)
