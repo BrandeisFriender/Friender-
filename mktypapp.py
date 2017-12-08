60a36f4556089abc89ec0a6452cc0e2292bd9b75
tim <tjhickey@brandeis.edu>
2017-12-08 10:24:51 -0500

    set production URL to 0.0.0.0

--- mktypapp.py
+++ mktypapp.py
@@ -229,4 +229,4 @@ def chat():
 
 if __name__ == '__main__':
 	app.run('127.0.0.1',port=5000) # DEVELOPMENT at 127.0.0.1:5000
-	#app.run('127.0.0.1',port=5500) # PRODUCTION at gracehopper.cs-i.brandeis.edu:5500
+	#app.run('0.0.0.0',port=5500) # PRODUCTION at gracehopper.cs-i.brandeis.edu:5500
